6e16aba243e6f45c08cc9f2deaf668ef373282dd
Alessio Moretti <alessio.moretti@live.it>
2018-06-20 21:22:50 +0200

    Init new Apache Flink project

---
+++ src/main/java/StormbreakerMain.java
@@ -0,0 +1,9 @@
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class StormbreakerMain {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+}
